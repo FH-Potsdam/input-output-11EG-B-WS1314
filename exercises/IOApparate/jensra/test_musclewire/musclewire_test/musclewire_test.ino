0ce7b4853e89396f529af85f9ce507d1f449fe58
jensra <jens.rauenbusch@fh-potsdam.de>
2014-01-31 19:04:11 +0100

    Musclewire test – it actually works!
    Got the musclewire to move.. but it doesn’t contract, rather expands to
    its original shape. (see video)

---
+++ exercises/IOApparate/jensra/test_musclewire/musclewire_test/musclewire_test.ino
@@ -0,0 +1,23 @@
+/**
+ * Simple push button sketch.
+ * Turns a LED on pin 13 on and off
+ *
+ */
+int ledpin = 13; // for output
+int pushpin = 2; // pushbutton input
+int is_on = 0;// boolean
+
+void setup(){
+pinMode(ledpin, OUTPUT); // init
+pinMode(pushpin, INPUT); // inti
+  
+}
+void loop(){
+  is_on = digitalRead(pushpin); // read from the button
+
+  if(is_on){
+  digitalWrite(ledpin,HIGH); // on
+  }else{
+    digitalWrite(ledpin,LOW); // off
+  }
+}
